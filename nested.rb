61c358a3d421e03c0cf81982a64f7d9728e8aa67
Plimon Howard V <plimon.howard@gmail.com>
2020-02-14 03:13:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
         }
      }
 		 programmer_hash[:alan_kay] = "GUI"
-		 
+
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 #     alans_new_info = "GUI"
 
